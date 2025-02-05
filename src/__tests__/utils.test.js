efd126a5b208c7d5067f3041d468b2f1ba81a4c4
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-05 19:49:15 +0300

    first test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import { isPalindrome } from '../utils'
+
+describe ('isPalindrome', ()=>{
+
+    it ("should return true if the current test is a palindrome", ()=>{
+        const test = isPalindrome
+        expect (isPalindrome('racecar')).toBeTruthy()
+    })
+
+    it ("should return false if the current test is not a palindrome", ()=>{
+        const test = isPalindrome
+        expect (isPalindrome('car')).toBeFalsy()
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+export function isPalindrome(word){
+    if(!word) return false;
+ let reversedWord = word.split('').reverse().join('');
+ return word === reversedWord;
+}
